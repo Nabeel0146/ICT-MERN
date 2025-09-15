1b8a75bbe0ad7b2ead0f40f11088aea135bd5d66
ziya zuhara <ziyazuhara77@gmail.com>
2025-09-15 12:25:29 +0530

    trying javascript

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Hello, World!");
+
+//let cannot be redeclared, var can be 
+let b=+10;
+
+console.log(b)
+
+var a=b+100;
+console.log(a)
+
+
+//const
+const c=120
+console.log(c)
+
+{
+    const c=130
+    console.log(c)
+}
