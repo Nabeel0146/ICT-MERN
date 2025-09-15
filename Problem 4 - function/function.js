5dc8a9be546ecaebd04ae7cb7bb620da7217486a
ziya zuhara <ziyazuhara77@gmail.com>
2025-09-15 14:50:30 +0530

    arrow function

--- Problem 4 - function/function.js
+++ Problem 4 - function/function.js
@@ -1,1 +1,17 @@
-console
+function adder(a,b){
+    sum=a+b
+    return sum
+}
+
+let result= adder(10,20)
+console.log(result)
+
+
+
+//arrow function 
+
+let adderfunction=(c,d)=>{
+    return c+d
+}
+
+console.log(adderfunction(20,40))
