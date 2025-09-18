08e29a765e85baa588e70a353ed0ac6fa18b5fc3
Nabeel0146 <muhammednabeelhere@gmail.com>
2025-09-18 16:23:51 +0530

    coommiitt

--- DAY3Actual/employeewebapp/src/Pages/ViewPage.jsx
+++ DAY3Actual/employeewebapp/src/Pages/ViewPage.jsx
@@ -1,9 +1,53 @@
+import { Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
 import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
 
 const ViewPage = () => {
-  return (
-    <div><h1>{name}</h1></div>
-  )
-}
 
-export default ViewPage
+ 
+    var [emp, setEmp] = useState([])
+    axios.get("http://localhost:3000/view")
+      .then((res) => {
+        setEmp(res.data)
+      })
+
+    const ddelete=(id)=>{
+      axios.delete(`http://localhost:3000/remove/${id}`)
+    }
+    
+
+    return (
+      <div><h1>View Data</h1>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Full Name</TableCell>
+                <TableCell>Department</TableCell>
+                <TableCell>Salary</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {emp.map((val) => (
+                <TableRow key={val._id}>
+                  <TableCell>{val.Name}</TableCell>
+                  <TableCell>{val.Age}</TableCell>
+                  <TableCell>{val.Dept}</TableCell>
+                  <TableCell>
+                    <Button variant='contained' color='error' onClick={()=> ddelete(val._id)}>Delete</Button>
+                  </TableCell>
+                </TableRow>)
+              )}
+              
+            </TableBody>
+          </Table>
+        </TableContainer>
+
+
+
+      </div>
+    )
+  }
+
+  export default ViewPage
